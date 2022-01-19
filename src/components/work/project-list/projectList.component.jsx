5b76ab60b495df1adcd1c40e474b166c1c9020fb
Shivam Bhasin <bhasinshivam2002@gmail.com>
2022-01-20 00:20:57 +0530

    made project list template and started adding some projects

---
+++ src/components/work/project-list/projectList.component.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+
+import './projectList.styles.scss';
+
+let { projects } = require('../../../assets/workPageData.json');
+
+const ProjectIcon = ({
+	id,
+	image,
+	name,
+	starred,
+	link,
+	repo,
+	...otherDetails
+}) => {
+	return (
+		<div className='project-icon'>
+			<div className='project-image'>
+				{image && (
+					<img
+						src={`${process.env.PUBLIC_URL}/assets/projectImages/${image}`}
+						alt={name + ' image'}
+					/>
+				)}
+			</div>
+			<div className='click-me'>Click Me</div>
+			<div className='project-details'>
+				{starred && <i className='fas fa-star' />}
+				<h6>{name}</h6>
+				{repo && (
+					<a href={repo} target='_blank' rel='noreferrer'>
+						<i className='fab fa-github' />
+					</a>
+				)}
+				{link && (
+					<a href={link} target='_blank' rel='noreferrer'>
+						<i className='fas fa-link' />
+					</a>
+				)}
+			</div>
+		</div>
+	);
+};
+
+const ProjectList = () => {
+	return (
+		<>
+			<div className='projects'>
+				<h1 className='heading'>Projects</h1>
+				<div className='list'>
+					{projects.map((project, i) => (
+						<ProjectIcon key={i} {...project} />
+					))}
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ProjectList;
--- src/pages/workPage/work.component.jsx
+++ src/pages/workPage/work.component.jsx
@@ -1,9 +1,13 @@
 import './work.styles.scss';
 
+import ProjectCarousel from '../../components/work/project-carousel/projectCarousel.component';
+import ProjectList from '../../components/work/project-list/projectList.component';
+
 const Work = () => {
 	return (
 		<div id='workPage'>
-			<section></section>
+			{/* <ProjectCarousel /> */}
+			<ProjectList />
 		</div>
 	);
 };
